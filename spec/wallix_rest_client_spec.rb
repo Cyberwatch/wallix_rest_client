f203672f3aafd5374b7eadf6837571c143b9dafd
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2018-08-29 09:42:05 +0200

    add basic unit tests

--- spec/wallix_rest_client_spec.rb
+++ spec/wallix_rest_client_spec.rb
@@ -1,31 +1,52 @@
+require 'base64'
+
 RSpec.describe WallixRestClient do
   it 'has a version number' do
     expect(WallixRestClient::VERSION).not_to be nil
   end
 
-  context 'WALLIX API CALLS - AUTH' do
-    xit 'can authenticate with :apikey mode' do
-      # Mockup Bittrex account for Rspec
-      WallixRestClient.setup do |config|
-        config.host = 'http://127.0.0.1/api'
-        config.mode = :apikey
-        config.user = 'login'
-        config.secret = 'apikey'
+  describe 'WALLIX API CALLS - AUTH' do
+    context 'can authenticate with :apikey mode' do
+      before(:all) do
+        WallixRestClient.setup do |config|
+          config.base_uri = 'http://127.0.0.1'
+          config.user = 'login'
+          config.secret = 'apikey'
+          config.options = {
+              auth: :apikey
+          }
+        end
       end
 
-      # TODO
+      let(:config) { config = WallixRestClient.configuration }
+      let(:uri) { uri = URI.parse([WallixRestClient.configuration.base_uri, '/api/'].join('')) }
+      let(:request) { request = WallixRestClient.build_request(Net::HTTP::Get, uri) }
+
+      it 'sets correctly the auth headers' do
+        expect(request['X-Auth-User']).to eq(config.user)
+        expect(request['X-Auth-Key']).to eq(config.secret)
+      end
     end
 
-    xit 'can authenticate with :basic mode' do
-      # Mockup Bittrex account for Rspec
-      WallixRestClient.setup do |config|
-        config.host = 'http://127.0.0.1/api'
-        config.mode = :basic
-        config.user = 'login'
-        config.secret = 'password'
+    context 'can authenticate with :basic mode' do
+      before(:all) do
+        WallixRestClient.setup do |config|
+          config.base_uri = 'http://127.0.0.1/api'
+          config.user = 'login'
+          config.secret = 'password'
+          config.options = {
+              auth: :basic
+          }
+        end
       end
 
-      # TODO
+      let(:config) { config = WallixRestClient.configuration }
+      let(:uri) { uri = URI.parse([WallixRestClient.configuration.base_uri, '/api/'].join('')) }
+      let(:request) { request = WallixRestClient.build_request(Net::HTTP::Get, uri) }
+
+      it 'sets correctly the auth headers' do
+        expect(request['Authorization']).to eq('Basic ' + Base64.encode64(config.user + ':' + config.secret).strip)
+      end
     end
   end
 end
